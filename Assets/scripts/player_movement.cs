db9311bab89bcdaf38e4bb8a9ed9fbb011574f1a
support <support@PC-8CG1242433.ad.jotunserver.com>
2024-09-14 17:56:30 +0200

    Added script level multiplayer support

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Spiller skal kunne gå fritt langs x og z aksen
+// Spiller skal kunne bytte mellom forskjellige entiter som er på lag
+// Spiller skal kunne ta flag
+// Spiller skal dø når den blir tatt av en motstander på dens territorie
+// Spiller skal kunne skade fiender på eget territorie
+
 public class player_movement : MonoBehaviour
 {
+    // State for spillerinformasjon
+    public int playerID;
+    
+    //[,] keybinds = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+    // State for bevegelse
     public float forceMultiplier = 200;
     float max_speed = 5f, acceleration = 1.1f;
+    public float drag = 0.95f;
     float xVelocity = 0f, zVelocity = 0f;
+    bool holding_key = false;
+
+    // Initialiserer unity klasser
     Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,28 +39,62 @@ public class player_movement : MonoBehaviour
     {
         if (rb.transform.position.y != 0.99)
         {
-            GetComponent<Rigidbody>().position = 
+            //GetComponent<Rigidbody>().position = 
         }
          // Sjekk input
-        if (Input.GetKey(KeyCode.W))
+        if (playerID == 0)
         {
-            zVelocity += acceleration;
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            xVelocity -= acceleration;
-        }
-        // Bestem bevegelse
+            if ( Input.GetKey(KeyCode.W))
+            {
+                zVelocity += acceleration;
+                holding_key = true;
+            }
+            
+            if (Input.GetKey(KeyCode.A))
+            {
+                xVelocity -= acceleration;
+                holding_key = true;
+            }
+            // Bestem bevegelse
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            zVelocity -= acceleration;
-        }
+            if (Input.GetKey(KeyCode.S))
+            {
+                zVelocity -= acceleration;
+                holding_key = true;
+            }
 
-        if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D))
+            {
+                xVelocity += acceleration;
+                holding_key = true;
+            }
+        }
+        if (playerID == 1)
         {
-            xVelocity += acceleration;
+            if ( Input.GetKey(KeyCode.UpArrow))
+            {
+                zVelocity += acceleration;
+                holding_key = true;
+            }
+            
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                xVelocity -= acceleration;
+                holding_key = true;
+            }
+            // Bestem bevegelse
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                zVelocity -= acceleration;
+                holding_key = true;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                xVelocity += acceleration;
+                holding_key = true;
+            }
         }
         if (xVelocity < -max_speed)
         {
@@ -60,7 +113,15 @@ public class player_movement : MonoBehaviour
         {
             zVelocity = max_speed;
         }
+
+        if (holding_key == false)
+        {
+            //acceleration = 0;
+            xVelocity *= drag;
+            zVelocity *= drag;
+        }
         rb.AddForce(new Vector3(xVelocity * forceMultiplier, 0, zVelocity * forceMultiplier));
+        holding_key = false;
     }
 
     private void FixedUpdate()
